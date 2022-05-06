73c2527aab49452805459b36f5d7e87e1f5a5f0d
Olivier Castro-Perrier <olivier.cperrier@hotmail.com>
2022-05-06 13:03:42 -0400

    fix: CLIN-1025 add search file query

--- src/graphql/patients/queries.tsx
+++ src/graphql/patients/queries.tsx
@@ -106,3 +106,60 @@ export const PATIENT_FILES_QUERY = (patientID: string) => gql`
     }
   }
 `;
+
+export const SEARCH_PATIENT_FILES_QUERY = (searchValue: string) => gql`
+  {
+    taskList: TaskList(
+      _filter: "(patient.identifier eq ${searchValue}) or (input_specimen.accession eq ${searchValue}) or ( run-name eq ${searchValue})"
+    ) {
+      id
+      focus @flatten {
+        serviceRequestReference: reference
+      }
+      owner @flatten {
+        owner: resource(type: Organization) {
+          id
+          alias @first @singleton
+          contact @flatten @first @singleton {
+            telecom @flatten @first @singleton {
+              email: value
+            }
+          }
+        }
+      }
+      output @flatten {
+        valueReference @flatten {
+          documents: resource(type: DocumentReference) {
+            contentList: content {
+              attachment {
+                url
+                hash64: hash
+                title
+              }
+              format @flatten {
+                fileFormat: code
+              }
+            }
+            context @flatten {
+              related @first @flatten {
+                sample: resource @flatten {
+                  accessionIdentifier @flatten {
+                    sampleId: value
+                  }
+                }
+              }
+            }
+            subject @flatten {
+              patientReference: reference
+            }
+            type @flatten {
+              coding @first @flatten {
+                fileType: code
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
