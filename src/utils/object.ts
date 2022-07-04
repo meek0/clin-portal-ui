cbbbc255ccc6c3a7622ce185b101c67e0fdd0f52
Matt Syms <matthieu.symoens@gmail.com>
2022-07-04 08:31:57 -0400

    feat: CLIN-1057 added object helper module

---
+++ src/utils/object.ts
@@ -0,0 +1,39 @@
+/**
+ * Simple object check
+ * @param item
+ * @returns {boolean}
+ */
+export const isObject = (item: any): boolean =>
+  item && typeof item === 'object' && !Array.isArray(item);
+
+/**
+ * Clone object
+ * @param item
+ * @returns {any}
+ */
+export const clone = (item: any): any => JSON.parse(JSON.stringify(item));
+
+/**
+ * Deep merge two objects
+ * @param target
+ * @param ...sources
+ */
+export const mergeDeep = <T = any>(target: any, ...sources: any[]): T => {
+  if (!sources.length) return target;
+  const source = sources.shift();
+
+  if (isObject(target) && isObject(source)) {
+    for (const key in source) {
+      if (isObject(source[key])) {
+        if (!target[key]) {
+          Object.assign(target, { [key]: {} });
+        }
+        mergeDeep(target[key], source[key]);
+      } else {
+        Object.assign(target, { [key]: source[key] });
+      }
+    }
+  }
+
+  return mergeDeep(target, ...sources);
+};
