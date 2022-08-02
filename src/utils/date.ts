6b0b6402c3125eb8c32c928d611a0477874e3fa7
Yann MARCOU <creativeyann17@gmail.com>
2022-08-02 14:06:39 -0400

    fix: CLIN-1207 formatDate is one day off

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,5 +1,6 @@
 export const formatDate = (date: string): string => {
-  const d = new Date(date);
+  // https://stackoverflow.com/questions/7556591/is-the-javascript-date-object-always-one-day-off
+  const d = new Date(date.replace(/-/g, '/').replace(/T.+/, ''));
   const month = `${d.getMonth() + 1}`.padStart(2, '0');
   const day = `${d.getDate()}`.padStart(2, '0');
   return `${d.getFullYear()}-${month}-${day}`;
