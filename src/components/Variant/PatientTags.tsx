e849f81c2e94bb6e4c69c14532dd9f52a66abae6
claudia1296 <claudia.roy.cr@gmail.com>
2022-10-31 14:38:15 -0400

    feat: CLIN-1407 ajout numero echantillon

--- src/components/Variant/PatientTags.tsx
+++ src/components/Variant/PatientTags.tsx
@@ -5,6 +5,36 @@ import { getPositionTag } from 'graphql/prescriptions/helper';
 
 import { useGlobals } from 'store/global';
 
+const getSpecimen = (
+  patientId: string,
+  prescription?: ServiceRequestEntity,
+  basedOnPrescription?: ServiceRequestEntity,
+) => {
+  const isParent = basedOnPrescription
+    ? basedOnPrescription.extensions?.some((p) =>
+        p.extension?.[1]?.valueReference?.reference.includes(patientId),
+      )
+    : false;
+
+  if (basedOnPrescription) {
+    if (isParent) {
+      const parentInfo = basedOnPrescription.extensions?.find((p) =>
+        p.extension?.[1]?.valueReference?.reference.includes(patientId),
+      );
+      return parentInfo?.extension?.[1]?.valueReference?.resource.requests[0].specimen[0].resource
+        .accessionIdentifier.value;
+    } else {
+      return basedOnPrescription?.subject?.resource?.requests?.[0].specimen?.find(
+        (specimen) => !('parent' in specimen.resource),
+      )?.resource.accessionIdentifier.value;
+    }
+  } else {
+    return prescription?.subject?.resource?.requests?.[0].specimen?.find(
+      (specimen) => !('parent' in specimen.resource),
+    )?.resource.accessionIdentifier.value;
+  }
+};
+
 export default (
   patientId: string,
   prescriptionId?: string,
@@ -13,12 +43,15 @@ export default (
 ): React.ReactNode[] => {
   const { getAnalysisNameByCode } = useGlobals();
 
+  const specimen = getSpecimen(patientId, prescription, basedOnPrescription);
   const tags: React.ReactNode[] = [
     <Tag color="blue" key="patient-prescription-id">
       <Space align="center">
         {`Patient ID : ${patientId}`}
         {prescriptionId && `|`}
         {prescriptionId && `Prescription ID : ${prescriptionId}`}
+        {specimen && `|`}
+        {specimen && `Échantillon : ${specimen}`}
       </Space>
     </Tag>,
   ];
