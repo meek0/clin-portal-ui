fb75b4ec6ad17ae41384bad1a8790bfb46632ecc
Yann MARCOU <creativeyann17@gmail.com>
2023-04-18 17:06:43 -0400

    fix: CLIN-1890 Graphql FHIR max count

--- src/providers/ApolloProvider.tsx
+++ src/providers/ApolloProvider.tsx
@@ -12,7 +12,7 @@ export const PROJECT_ID = EnvironmentVariables.configFor('ARRANGER_PROJECT_ID');
 export const FHIR_API = EnvironmentVariables.configFor('FHIR_API');
 
 export const ARRANGER_API_PROJECT_URL = `${ARRANGER_API}/${PROJECT_ID}/graphql`;
-export const FHIR_GRAPHQL_URL = `${FHIR_API}/$graphql`;
+export const FHIR_GRAPHQL_URL = `${FHIR_API}/$graphql?_count=1000`;
 
 const fhirLink = createHttpLink({
   uri: FHIR_GRAPHQL_URL,
