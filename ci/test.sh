63c17613b017713401d28bbd5dc283f0d4e91ce3
Issam Hannache <ihannache@ferlab.bio>
2023-05-05 08:33:03 -0400

    INFRA-360 Removed test script for portal ui pipelines

--- ci/test.sh
+++
@@ -1,7 +0,0 @@
-          
-branchName=clin-123
-if ! [[ sed-4.2.2.${branchName} =~ ([a-z]{2,8}-[0-9]+[a-z0-9\-]) ]]
-          then
-            echo "Le nom de branche contient des caratere spa"
-            exit 1
-          fi
