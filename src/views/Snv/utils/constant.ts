af00013272a4d233510857a2effc20db79f2d04a
ethienneroy <ethienner@gmail.com>
2023-07-04 15:02:40 -0400

    fix: CLIN-2019 activated sort on exomiser score column

--- src/views/Snv/utils/constant.ts
+++ src/views/Snv/utils/constant.ts
@@ -18,6 +18,7 @@ export const DEFAULT_PAGING_CONFIG = {
 };
 
 export const DEFAULT_SORT_QUERY = [
+  { field: 'donors.exomiser.gene_combined_score', order: SortDirection.Desc },
   { field: 'max_impact_score', order: SortDirection.Desc },
   { field: 'hgvsg', order: SortDirection.Asc },
 ] as ISort[];
