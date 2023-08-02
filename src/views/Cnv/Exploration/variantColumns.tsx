6e6df6430e7a1663c38694d55b9a3774275d3315
Yann MARCOU <creativeyann17@gmail.com>
2023-08-02 09:58:25 -0400

    feat: CLIN-2098 SM optional

--- src/views/Cnv/Exploration/variantColumns.tsx
+++ src/views/Cnv/Exploration/variantColumns.tsx
@@ -86,7 +86,7 @@ export const getVariantColumns = (
       key: 'sm',
       dataIndex: 'sm',
       sorter: { multiple: 1 },
-      defaultHidden: true,
+      defaultHidden: variantType === VariantType.GERMLINE,
       render: (sm: string) => sm,
       width: 75,
     },
