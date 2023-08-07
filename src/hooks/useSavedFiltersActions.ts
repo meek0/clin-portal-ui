c4955f6687d177f4d049e33224974a9fe52ed06c
ethienneroy <ethienner@gmail.com>
2023-08-07 10:24:42 -0400

    fix: CLIN-2107 fixed broken shared filter navigation

--- src/hooks/useSavedFiltersActions.ts
+++ src/hooks/useSavedFiltersActions.ts
@@ -7,6 +7,8 @@ import { createSavedFilter, deleteSavedFilter, updateSavedFilter } from 'store/s
 import { SHARED_FILTER_ID_QUERY_PARAM_KEY } from 'utils/constants';
 import { getCurrentUrl } from 'utils/helper';
 
+import { TUserSavedFilter } from '../api/savedFilter/models';
+
 const useSavedFiltersActions = (savedFilterTag: string) => {
   const dispatch = useAppDispatch();
 
@@ -22,8 +24,12 @@ const useSavedFiltersActions = (savedFilterTag: string) => {
 
   const handleOnDeleteFilter = (id: string) => dispatch(deleteSavedFilter(id));
 
-  const handleOnShareFilter = (filter: ISavedFilter) => {
-    copy(`${getCurrentUrl()}?${SHARED_FILTER_ID_QUERY_PARAM_KEY}=${filter.id}`);
+  const handleOnShareFilter = (filter: TUserSavedFilter) => {
+    let url = `${getCurrentUrl()}?${SHARED_FILTER_ID_QUERY_PARAM_KEY}=${filter.id}`;
+    if (filter.tag?.includes('patient')) {
+      url = url.concat(`&variantSection=${filter.tag.split('_')[0]}`).concat('#variants');
+    }
+    copy(url);
     dispatch(
       globalActions.displayMessage({
         content: 'Copied share url',
