424ad36971a9282183eefc0e5782488716c17dd1
ethienneroy <ethienner@gmail.com>
2023-08-22 14:10:33 -0400

    fix: CLIN-1913 better handling of or sqon queries when prescriptionID…

--- src/views/Snv/utils/helper.ts
+++ src/views/Snv/utils/helper.ts
@@ -1,4 +1,4 @@
-import { TermOperators } from '@ferlab/ui/core/data/sqon/operators';
+import { BooleanOperators, TermOperators } from '@ferlab/ui/core/data/sqon/operators';
 import { ISqonGroupFilter } from '@ferlab/ui/core/data/sqon/types';
 
 export const wrapSqonWithDonorIdAndSrId = (
@@ -6,51 +6,88 @@ export const wrapSqonWithDonorIdAndSrId = (
   patientId?: string,
   prescriptionId?: string,
 ) => {
+  const cleanSqon = (sqon: any, keyToRemove = 'pivot') => {
+    if (typeof sqon !== 'object' || sqon === null) {
+      throw new Error('Input is not a valid JSON object.');
+    }
+
+    if (keyToRemove in sqon) {
+      delete sqon[keyToRemove];
+    }
+
+    for (const key in sqon) {
+      if (typeof sqon[key] === 'object') {
+        cleanSqon(sqon[key], keyToRemove);
+      }
+    }
+
+    return sqon;
+  };
+
+  const addFilters = (content: any, patientFilter: any, prescriptionFilter: any) => {
+    if (patientFilter) content.push(patientFilter);
+    if (prescriptionFilter) content.push(prescriptionFilter);
+    return content;
+  };
+  const handleContent = (sqon: any, patientFilter: any, prescriptionFilter: any): any => {
+    // @ts-ignore
+    const explodeOrCondition = (toExplode: any) => {
+      toExplode.content = toExplode.content.map((item: any) => ({
+        content: addFilters(
+          Array.isArray(item.content) ? item.content : [item],
+          patientFilter,
+          prescriptionFilter,
+        ),
+        op: 'and',
+        pivot: 'donors',
+      }));
+
+      return toExplode;
+    };
+
+    if (sqon && JSON.stringify(sqon).includes('"op":"or"')) {
+      // go recursive to update it
+      if (
+        (sqon.op === BooleanOperators.or || sqon.op === BooleanOperators.and) &&
+        !JSON.stringify(sqon.content).includes('"op":"or"')
+      ) {
+        return explodeOrCondition(sqon);
+      } else if (Array.isArray(sqon.content)) {
+        sqon.content = sqon.content.map((item: any) =>
+          handleContent(item, patientFilter, prescriptionFilter),
+        );
+      }
+    } else if (sqon && Array.isArray(sqon.content)) {
+      return {
+        ...sqon,
+        content: addFilters(sqon.content, patientFilter, prescriptionFilter),
+        pivot: 'donors',
+      };
+    } else {
+      return {
+        ...sqon,
+        content: addFilters([sqon], patientFilter, prescriptionFilter),
+        pivot: 'donors',
+        op: 'and',
+      };
+    }
+    return sqon;
+  };
+
+  const prescriptionFilter = prescriptionId
+    ? {
+        content: { field: 'donors.service_request_id', value: [prescriptionId] },
+        op: TermOperators.in,
+      }
+    : null;
+
+  const patientFilter = patientId
+    ? { content: { field: 'donors.patient_id', value: [patientId] }, op: TermOperators.in }
+    : null;
+
   if (patientId || prescriptionId) {
-    const subContent: any[] = [];
-
-    const prescriptionFilter = {
-      content: { field: 'donors.service_request_id', value: [prescriptionId] },
-      op: TermOperators.in,
-    };
-
-    const patientFilter = {
-      content: { field: 'donors.patient_id', value: [patientId] },
-      op: TermOperators.in,
-    };
-
-    if (patientId) {
-      subContent.push(patientFilter);
-    }
-
-    if (prescriptionId) {
-      subContent.push(prescriptionFilter);
-    }
-
-    if (resolvedSqon.op === 'or') {
-      resolvedSqon.content.forEach((sqon: any) => {
-        if (prescriptionId) {
-          sqon.content?.push(prescriptionFilter);
-        }
-        if (patientId) {
-          sqon.content?.push(patientFilter);
-        }
-
-        sqon.pivot = 'donors';
-      });
-      return resolvedSqon;
-    }
-    return {
-      content: [
-        {
-          content: subContent,
-          op: 'and',
-        },
-        { ...resolvedSqon },
-      ],
-      op: 'and',
-      pivot: 'donors',
-    };
+    const cleanedSqon = cleanSqon(resolvedSqon);
+    return handleContent(cleanedSqon, patientFilter, prescriptionFilter);
   }
 
   return resolvedSqon;
--- src/views/Snv/utils/tests/helper.spec.js
+++ src/views/Snv/utils/tests/helper.spec.js
@@ -1,32 +1,36 @@
 import { wrapSqonWithDonorIdAndSrId } from '../helper';
 
 describe('wrapSqonWithDonorIdAndSrId', () => {
-  test('Should wrap patientId', () => {
-    const resolvedSqon = {};
+  beforeAll(() => {
+    // eslint-disable-next-line no-undef
+    global.structuredClone = (v) => ({ ...v });
+  });
 
+  test('Should wrap patientId', () => {
+    const resolvedSqon = {
+      content: [],
+      op: 'and',
+      pivot: 'donors',
+    };
     const expected = {
-      content: [
-        {
-          content: [{ content: { field: 'donors.patient_id', value: ['foo'] }, op: 'in' }],
-          op: 'and',
-        },
-        {},
-      ],
+      content: [{ content: { field: 'donors.patient_id', value: ['foo'] }, op: 'in' }],
       op: 'and',
       pivot: 'donors',
     };
     expect(wrapSqonWithDonorIdAndSrId(resolvedSqon, 'foo', null)).toEqual(expected);
   });
   test('Should wrap prescriptionId', () => {
-    const resolvedSqon = {};
-
+    const resolvedSqon = {
+      content: [],
+      op: 'and',
+      pivot: 'donors',
+    };
     const expected = {
       content: [
         {
-          content: [{ content: { field: 'donors.service_request_id', value: ['bar'] }, op: 'in' }],
-          op: 'and',
+          content: { field: 'donors.service_request_id', value: ['bar'] },
+          op: 'in',
         },
-        {},
       ],
       op: 'and',
       pivot: 'donors',
@@ -34,22 +38,597 @@ describe('wrapSqonWithDonorIdAndSrId', () => {
     expect(wrapSqonWithDonorIdAndSrId(resolvedSqon, null, 'bar')).toEqual(expected);
   });
   test('Should wrap both patient and prescription', () => {
-    const resolvedSqon = {};
-
+    const resolvedSqon = {
+      content: [],
+      op: 'and',
+      pivot: 'donors',
+    };
     const expected = {
       content: [
-        {
-          content: [
-            { content: { field: 'donors.patient_id', value: ['foo'] }, op: 'in' },
-            { content: { field: 'donors.service_request_id', value: ['bar'] }, op: 'in' },
-          ],
-          op: 'and',
-        },
-        {},
+        { content: { field: 'donors.patient_id', value: ['foo'] }, op: 'in' },
+        { content: { field: 'donors.service_request_id', value: ['bar'] }, op: 'in' },
       ],
       op: 'and',
       pivot: 'donors',
     };
     expect(wrapSqonWithDonorIdAndSrId(resolvedSqon, 'foo', 'bar')).toEqual(expected);
+  });
+
+  test('should wrap patientID with empty content', () => {
+    const resolvedSqon = {
+      content: [],
+      op: 'and',
+      pivot: 'donors',
+    };
+    const expected = {
+      content: [{ content: { field: 'donors.patient_id', value: ['foo'] }, op: 'in' }],
+      op: 'and',
+      pivot: 'donors',
+    };
+    expect(wrapSqonWithDonorIdAndSrId(resolvedSqon, 'foo', null)).toEqual(expected);
+  });
+
+  test('wrap or', () => {
+    const initialSqon = {
+      content: [
+        {
+          content: {
+            field: 'consequences.biotype',
+            index: 'Variants',
+            value: ['processed_pseudogene'],
+          },
+          op: 'in',
+        },
+        {
+          content: {
+            field: 'gene_external_reference',
+            index: 'Variants',
+            value: ['HPO'],
+          },
+          op: 'in',
+        },
+      ],
+      op: 'or',
+    };
+
+    const expected = {
+      content: [
+        {
+          content: [
+            {
+              content: {
+                field: 'consequences.biotype',
+                index: 'Variants',
+                value: ['processed_pseudogene'],
+              },
+              op: 'in',
+            },
+            { content: { field: 'donors.patient_id', value: ['foo'] }, op: 'in' },
+          ],
+          op: 'and',
+          pivot: 'donors',
+        },
+        {
+          content: [
+            {
+              content: {
+                field: 'gene_external_reference',
+                index: 'Variants',
+                value: ['HPO'],
+              },
+              op: 'in',
+            },
+            { content: { field: 'donors.patient_id', value: ['foo'] }, op: 'in' },
+          ],
+          op: 'and',
+          pivot: 'donors',
+        },
+      ],
+      op: 'or',
+    };
+    expect(wrapSqonWithDonorIdAndSrId(initialSqon, 'foo', null)).toEqual(expected);
+  });
+
+  test('unique query', () => {
+    const initialSqon = {
+      content: [
+        {
+          content: {
+            field: 'donors.gq',
+            index: 'Variants',
+            value: ['__missing__'],
+          },
+          op: 'in',
+        },
+      ],
+      op: 'and',
+      pivot: 'donors',
+    };
+    const expected = {
+      content: [
+        {
+          content: {
+            field: 'donors.gq',
+            index: 'Variants',
+            value: ['__missing__'],
+          },
+          op: 'in',
+        },
+        { content: { field: 'donors.patient_id', value: ['foo'] }, op: 'in' },
+      ],
+      op: 'and',
+      pivot: 'donors',
+    };
+    expect(wrapSqonWithDonorIdAndSrId(initialSqon, 'foo', null)).toEqual(expected);
+  });
+
+  test('combined or queries', () => {
+    const initialSqon = {
+      content: [
+        {
+          content: [
+            { content: { field: 'donors.gq', index: 'Variants', value: [20] }, op: '>' },
+            {
+              content: { field: 'donors.gq', index: 'Variants', value: ['__missing__'] },
+              op: 'in',
+            },
+          ],
+          op: 'or',
+        },
+      ],
+      op: 'and',
+      pivot: 'donors',
+    };
+
+    const expected = {
+      content: [
+        {
+          content: [
+            {
+              content: [
+                {
+                  content: {
+                    field: 'donors.gq',
+                    index: 'Variants',
+                    value: [20],
+                  },
+                  op: '>',
+                },
+                {
+                  content: {
+                    field: 'donors.patient_id',
+                    value: ['foo'],
+                  },
+                  op: 'in',
+                },
+              ],
+              op: 'and',
+              pivot: 'donors',
+            },
+            {
+              content: [
+                {
+                  content: {
+                    field: 'donors.gq',
+                    index: 'Variants',
+                    value: ['__missing__'],
+                  },
+                  op: 'in',
+                },
+                {
+                  content: {
+                    field: 'donors.patient_id',
+                    value: ['foo'],
+                  },
+                  op: 'in',
+                },
+              ],
+              op: 'and',
+              pivot: 'donors',
+            },
+          ],
+          op: 'or',
+        },
+      ],
+      op: 'and',
+      pivot: 'donors',
+    };
+    expect(wrapSqonWithDonorIdAndSrId(initialSqon, 'foo', null)).toEqual(expected);
+  });
+
+  test('a complex one', () => {
+    const initialSqon = {
+      content: [
+        {
+          content: [
+            {
+              content: [
+                {
+                  content: {
+                    field: 'donors.gq',
+                    index: 'Variants',
+                    value: [20],
+                  },
+                  op: '>',
+                },
+              ],
+              op: 'and',
+            },
+            {
+              content: [
+                {
+                  content: {
+                    field: 'donors.gq',
+                    index: 'Variants',
+                    value: ['__missing__'],
+                  },
+                  op: 'in',
+                },
+              ],
+              op: 'and',
+            },
+          ],
+          op: 'or',
+        },
+        {
+          content: [
+            {
+              content: {
+                field: 'consequences.biotype',
+                index: 'Variants',
+                value: ['processed_pseudogene', 'unprocessed_pseudogene', 'processed_transcript'],
+              },
+              op: 'in',
+            },
+          ],
+          op: 'and',
+        },
+      ],
+      op: 'and',
+      pivot: 'donors',
+    };
+
+    const expected = {
+      content: [
+        {
+          content: [
+            {
+              content: [
+                {
+                  content: {
+                    field: 'donors.gq',
+                    index: 'Variants',
+                    value: [20],
+                  },
+                  op: '>',
+                },
+                {
+                  content: {
+                    field: 'donors.patient_id',
+                    value: ['foo'],
+                  },
+                  op: 'in',
+                },
+              ],
+              op: 'and',
+              pivot: 'donors',
+            },
+            {
+              content: [
+                {
+                  content: {
+                    field: 'donors.gq',
+                    index: 'Variants',
+                    value: ['__missing__'],
+                  },
+                  op: 'in',
+                },
+                {
+                  content: {
+                    field: 'donors.patient_id',
+                    value: ['foo'],
+                  },
+                  op: 'in',
+                },
+              ],
+              op: 'and',
+              pivot: 'donors',
+            },
+          ],
+          op: 'or',
+        },
+        {
+          content: [
+            {
+              content: {
+                field: 'consequences.biotype',
+                index: 'Variants',
+                value: ['processed_pseudogene', 'unprocessed_pseudogene', 'processed_transcript'],
+              },
+              op: 'in',
+            },
+            {
+              content: {
+                field: 'donors.patient_id',
+                value: ['foo'],
+              },
+              op: 'in',
+            },
+          ],
+          op: 'and',
+          pivot: 'donors',
+        },
+      ],
+      op: 'and',
+      pivot: 'donors',
+    };
+    expect(wrapSqonWithDonorIdAndSrId(initialSqon, 'foo', null)).toEqual(expected);
+  });
+
+  test('a complex query containing AND/OR', () => {
+    const initialSqon = {
+      content: [
+        {
+          content: [
+            {
+              content: {
+                field: 'donors.gq',
+                index: 'Variants',
+                value: [200],
+              },
+              op: '>=',
+            },
+            {
+              content: {
+                field: 'donors.gq',
+                index: 'Variants',
+                value: ['__missing__'],
+              },
+              op: 'in',
+            },
+          ],
+          op: 'or',
+        },
+        {
+          content: [
+            {
+              content: {
+                field: 'donors.ad_alt',
+                index: 'Variants',
+                value: [500],
+              },
+              op: '>=',
+            },
+            {
+              content: {
+                field: 'donors.ad_alt',
+                index: 'Variants',
+                value: ['__missing__'],
+              },
+              op: 'in',
+            },
+          ],
+          op: 'or',
+        },
+      ],
+      op: 'or',
+      pivot: 'donors',
+    };
+
+    const expected = {
+      content: [
+        {
+          content: [
+            {
+              content: [
+                {
+                  content: {
+                    field: 'donors.gq',
+                    index: 'Variants',
+                    value: [200],
+                  },
+                  op: '>=',
+                },
+                {
+                  content: {
+                    field: 'donors.patient_id',
+                    value: ['foo'],
+                  },
+                  op: 'in',
+                },
+              ],
+              op: 'and',
+              pivot: 'donors',
+            },
+            {
+              content: [
+                {
+                  content: {
+                    field: 'donors.gq',
+                    index: 'Variants',
+                    value: ['__missing__'],
+                  },
+                  op: 'in',
+                },
+                {
+                  content: {
+                    field: 'donors.patient_id',
+                    value: ['foo'],
+                  },
+                  op: 'in',
+                },
+              ],
+              op: 'and',
+              pivot: 'donors',
+            },
+          ],
+          op: 'or',
+        },
+        {
+          content: [
+            {
+              content: [
+                {
+                  content: {
+                    field: 'donors.ad_alt',
+                    index: 'Variants',
+                    value: [500],
+                  },
+                  op: '>=',
+                },
+                {
+                  content: {
+                    field: 'donors.patient_id',
+                    value: ['foo'],
+                  },
+                  op: 'in',
+                },
+              ],
+              op: 'and',
+              pivot: 'donors',
+            },
+            {
+              content: [
+                {
+                  content: {
+                    field: 'donors.ad_alt',
+                    index: 'Variants',
+                    value: ['__missing__'],
+                  },
+                  op: 'in',
+                },
+                {
+                  content: {
+                    field: 'donors.patient_id',
+                    value: ['foo'],
+                  },
+                  op: 'in',
+                },
+              ],
+              op: 'and',
+              pivot: 'donors',
+            },
+          ],
+          op: 'or',
+        },
+      ],
+      op: 'or',
+      pivot: 'donors',
+    };
+
+    expect(wrapSqonWithDonorIdAndSrId(initialSqon, 'foo', null)).toEqual(expected);
+  });
+
+  test('handles complex query', () => {
+    const initialSqon = {
+      content: [
+        {
+          content: [
+            {
+              content: {
+                field: 'donors.gq',
+                index: 'Variants',
+                value: [20],
+              },
+              op: '<',
+            },
+            {
+              content: {
+                field: 'donors.gq',
+                index: 'Variants',
+                value: ['__missing__'],
+              },
+              op: 'in',
+            },
+          ],
+          op: 'or',
+        },
+        {
+          content: {
+            field: 'donors.transmission',
+            index: 'Variants',
+            value: ['autosomal_dominant_de_novo'],
+          },
+          op: 'in',
+        },
+      ],
+      op: 'and',
+    };
+
+    const expected = {
+      content: [
+        {
+          content: [
+            {
+              content: [
+                {
+                  content: {
+                    field: 'donors.gq',
+                    index: 'Variants',
+                    value: [20],
+                  },
+                  op: '<',
+                },
+                {
+                  content: {
+                    field: 'donors.patient_id',
+                    value: ['922552'],
+                  },
+                  op: 'in',
+                },
+              ],
+              op: 'and',
+              pivot: 'donors',
+            },
+            {
+              content: [
+                {
+                  content: {
+                    field: 'donors.gq',
+                    index: 'Variants',
+                    value: ['__missing__'],
+                  },
+                  op: 'in',
+                },
+                {
+                  content: {
+                    field: 'donors.patient_id',
+                    value: ['922552'],
+                  },
+                  op: 'in',
+                },
+              ],
+              op: 'and',
+              pivot: 'donors',
+            },
+          ],
+          op: 'or',
+        },
+        {
+          content: [
+            {
+              content: {
+                field: 'donors.transmission',
+                index: 'Variants',
+                value: ['autosomal_dominant_de_novo'],
+              },
+              op: 'in',
+            },
+            {
+              content: {
+                field: 'donors.patient_id',
+                value: ['922552'],
+              },
+              op: 'in',
+            },
+          ],
+          op: 'and',
+          pivot: 'donors',
+        },
+      ],
+      op: 'and',
+    };
+
+    expect(wrapSqonWithDonorIdAndSrId(initialSqon, '922552', null)).toEqual(expected);
   });
 });
