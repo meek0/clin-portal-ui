4c0f6b9230768b804061377e0212735f848020a8
claudia1296 <claudia.roy.cr@gmail.com>
2024-10-17 13:08:15 -0400

    feat: CLIN-2989 fix or

--- src/views/Snv/Exploration/components/Flag/FlagFilter.tsx
+++ src/views/Snv/Exploration/components/Flag/FlagFilter.tsx
@@ -41,6 +41,11 @@ const noFlagQuery = (filteredValues: string[]) => ({
   op: 'or',
 });
 
+const activeQueryContent = (activeQuery: any) => ({
+  content: activeQuery!.content.length > 0 ? [activeQuery] : [],
+  op: 'and',
+});
+
 export const newQuery = (activeQuery: ISyntheticSqon, values: string[]) => {
   const filteredValues = values.filter((v) => v !== 'none');
   const activeQueryWithoutFilter =
@@ -49,18 +54,32 @@ export const newQuery = (activeQuery: ISyntheticSqon, values: string[]) => {
       : activeQuery;
 
   const queries = [...activeQueryWithoutFilter.content];
+  let asFlag = false;
+  activeQuery.content.forEach((c: any) => {
+    if (c.content) {
+      asFlag = c.content.field === 'flags';
+    }
+  });
+  const queriesNew: any =
+    values.length > 0 && !asFlag
+      ? [activeQueryContent(activeQueryWithoutFilter)]
+      : [...activeQueryWithoutFilter.content];
 
   values.length > 0 &&
     queries.push(
       values.includes('none') ? noFlagQuery(filteredValues) : flagFilterQuery(filteredValues),
     );
 
+  values.length > 0 &&
+    queriesNew.push(
+      values.includes('none') ? noFlagQuery(filteredValues) : flagFilterQuery(filteredValues),
+    );
+
   const isDuplicate = (obj: TSyntheticSqonContentValue, array: any[]) =>
     array.some((item: any) => JSON.stringify(item) === JSON.stringify(obj));
-
   const uniqueArray: any = [];
 
-  queries.forEach((item: any) => {
+  queriesNew.forEach((item: any) => {
     if (!isDuplicate(item, uniqueArray)) {
       uniqueArray.push(item);
     }
