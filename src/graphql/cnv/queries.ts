fb38b080156e27c640dc072e5f5fe3da1c980864
Yann MARCOU <creativeyann17@gmail.com>
2024-11-15 14:12:54 -0500

    fix: CLIN-33510 count no items

--- src/graphql/cnv/queries.ts
+++ src/graphql/cnv/queries.ts
@@ -3,13 +3,8 @@ import { gql } from '@apollo/client';
 export const GET_VARIANT_COUNT = gql`
   query CnvCount($sqon: JSON) {
     cnv {
-      hits(filters: $sqon) {
+      hits(filters: $sqon, first: 0) {
         total
-        edges {
-          node {
-            id
-          }
-        }
       }
     }
   }
