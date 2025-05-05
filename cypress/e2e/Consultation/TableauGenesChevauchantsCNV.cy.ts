5392497641dfc32c444e06894f5b3c703b0c67e6
kstonge <karine.st-onge.hsj@ssss.gouv.qc.ca>
2025-05-05 06:30:26 -0400

    feat: CLIN-4350 Adjust Cypress tests

--- cypress/e2e/Consultation/TableauGenesChevauchantsCNV.cy.ts
+++ cypress/e2e/Consultation/TableauGenesChevauchantsCNV.cy.ts
@@ -15,28 +15,37 @@ describe('Modal des gènes chevauchant un CNV - Consultation du tableau', () => 
 
     cy.get('div[role="dialog"] tr[data-row-key="OCLN"] td').eq(0).contains('OCLN').should('exist');
     cy.get('div[role="dialog"] tr[data-row-key="OCLN"] td').eq(1).contains('5q13.2').should('exist');
-    cy.get('div[role="dialog"] tr[data-row-key="OCLN"] td').eq(2).find('[class*="anticon"]').should('exist');
-    cy.get('div[role="dialog"] tr[data-row-key="OCLN"] td').eq(3).contains('RGDI').should('exist');
-    cy.get('div[role="dialog"] tr[data-row-key="OCLN"] td').eq(4).contains('65.6 kb').should('exist');
-    cy.get('div[role="dialog"] tr[data-row-key="OCLN"] td').eq(5).contains('6.6 kb').should('exist');
-    cy.get('div[role="dialog"] tr[data-row-key="OCLN"] td').eq(6).contains(/^2$/).should('exist');
-    cy.get('div[role="dialog"] tr[data-row-key="OCLN"] td').eq(7).contains('10.1 %').should('exist');
-    cy.get('div[role="dialog"] tr[data-row-key="OCLN"] td').eq(8).contains('24.2 %').should('exist');
-    cy.get('div[role="dialog"] tr[data-row-key="OCLN"] td').eq(9).find('path[d*="M12.4"]').should('exist'); // Gène recoupe le CNV à une de ses extrémités
-    cy.get('div[role="dialog"] tr[data-row-key="OCLN"] td').eq(9).find('path[d*="M15.6"]').should('exist');
+    cy.get('div[role="dialog"] tr[data-row-key="OCLN"] td').eq(2).contains('AR').should('exist');
+    cy.get('div[role="dialog"] tr[data-row-key="OCLN"] td').eq(2).find('[class*="ant-tag-blue"]').should('exist');
+    cy.get('div[role="dialog"] tr[data-row-key="OCLN"] td').eq(3).find('[class*="anticon"]').should('exist');
+    cy.get('div[role="dialog"] tr[data-row-key="OCLN"] td').eq(4).contains('RGDI').should('exist');
+    cy.get('div[role="dialog"] tr[data-row-key="OCLN"] td').eq(5).contains('65.6 kb').should('exist');
+    cy.get('div[role="dialog"] tr[data-row-key="OCLN"] td').eq(6).contains('6.6 kb').should('exist');
+    cy.get('div[role="dialog"] tr[data-row-key="OCLN"] td').eq(7).contains(/^2$/).should('exist');
+    cy.get('div[role="dialog"] tr[data-row-key="OCLN"] td').eq(8).contains('10.1 %').should('exist');
+    cy.get('div[role="dialog"] tr[data-row-key="OCLN"] td').eq(9).contains('24.2 %').should('exist');
+    cy.get('div[role="dialog"] tr[data-row-key="OCLN"] td').eq(10).find('path[d*="M12.4"]').should('exist'); // Gène recoupe le CNV à une de ses extrémités
+    cy.get('div[role="dialog"] tr[data-row-key="OCLN"] td').eq(10).find('path[d*="M15.6"]').should('exist');
   });
  
   it('Valider les liens disponibles Lien Gène', () => {
     cy.get('tr').contains(/^2$/).clickAndWait({force: true});
 
     cy.get('div[role="dialog"] tr[data-row-key="OCLN"]').contains('OCLN')
-      .should('have.attr', 'href', 'https://www.omim.org/search?index=entry&start=1&limit=10&sort=score+desc%2C+prefix_sort+desc&search=OCLN');
+      .should('have.attr', 'href', 'https://www.omim.org/entry/602876');
+  });
+ 
+  it('Valider les liens disponibles Lien OMIM', () => {
+    cy.get('tr').contains(/^2$/).clickAndWait({force: true});
+    
+    cy.get('div[role="dialog"] tr[data-row-key="OCLN"]').find('td').eq(2).find('a[href]')
+      .should('have.attr', 'href', 'https://www.omim.org/entry/602876');
   });
  
   it('Valider les liens disponibles Lien ClinGen', () => {
     cy.get('tr').contains(/^2$/).clickAndWait({force: true});
     
-    cy.get('div[role="dialog"] tr[data-row-key="OCLN"]').find('td').eq(2).find('a[href]')
+    cy.get('div[role="dialog"] tr[data-row-key="OCLN"]').find('td').eq(3).find('a[href]')
       .should('have.attr', 'href', 'https://search.clinicalgenome.org/kb/genes?page=1&size=50&search=OCLN');
   });
 
@@ -49,60 +58,69 @@ describe('Modal des gènes chevauchant un CNV - Consultation du tableau', () => 
     cy.get('div[role="dialog"] tr[class*="ant-table-row"]').eq(0).find('td').eq(0).contains('OCLN').should('exist');
   });
 
+  it('Valider les fonctionnalités du tableau - Tri OMIM', () => {
+    cy.get('tr').contains(/^2$/).clickAndWait({force: true});
+
+    cy.get('div[role="dialog"] thead[class="ant-table-thead"]').contains('OMIM').clickAndWait({force: true});
+    cy.get('div[role="dialog"] tr[class*="ant-table-row"]').eq(0).find('td').eq(2).contains('-').should('exist');
+    cy.get('div[role="dialog"] thead[class="ant-table-thead"]').contains('OMIM').clickAndWait({force: true});
+    cy.get('div[role="dialog"] tr[class*="ant-table-row"]').eq(0).find('td').eq(2).contains('AR').should('exist');
+  });
+
   it('Valider les fonctionnalités du tableau - Tri Longueur du gène', () => {
     cy.get('tr').contains(/^2$/).clickAndWait({force: true});
 
     cy.get('div[role="dialog"] thead[class="ant-table-thead"]').contains('Longueur du gène').clickAndWait({force: true});
-    cy.get('div[role="dialog"] tr[class*="ant-table-row"]').eq(0).find('td').eq(4).contains('35.0 kb').should('exist');
+    cy.get('div[role="dialog"] tr[class*="ant-table-row"]').eq(0).find('td').eq(5).contains('35.0 kb').should('exist');
     cy.get('div[role="dialog"] thead[class="ant-table-thead"]').contains('Longueur du gène').clickAndWait({force: true});
-    cy.get('div[role="dialog"] tr[class*="ant-table-row"]').eq(0).find('td').eq(4).contains('65.6 kb').should('exist');
+    cy.get('div[role="dialog"] tr[class*="ant-table-row"]').eq(0).find('td').eq(5).contains('65.6 kb').should('exist');
   });
 
   it('Valider les fonctionnalités du tableau - Tri # Bases', () => {
     cy.get('tr').contains(/^2$/).clickAndWait({force: true});
 
     cy.get('div[role="dialog"] thead[class="ant-table-thead"]').contains('# Bases').clickAndWait({force: true});
-    cy.get('div[role="dialog"] tr[class*="ant-table-row"]').eq(0).find('td').eq(5).contains('6.6 kb').should('exist');
+    cy.get('div[role="dialog"] tr[class*="ant-table-row"]').eq(0).find('td').eq(6).contains('6.6 kb').should('exist');
     cy.get('div[role="dialog"] thead[class="ant-table-thead"]').contains('# Bases').clickAndWait({force: true});
-    cy.get('div[role="dialog"] tr[class*="ant-table-row"]').eq(0).find('td').eq(5).contains('18.6 kb').should('exist');
+    cy.get('div[role="dialog"] tr[class*="ant-table-row"]').eq(0).find('td').eq(6).contains('18.6 kb').should('exist');
   });
 
   it('Valider les fonctionnalités du tableau - Tri # Exons', () => {
     cy.get('tr').contains(/^2$/).clickAndWait({force: true});
 
     cy.get('div[role="dialog"] thead[class="ant-table-thead"]').contains('# Exons').clickAndWait({force: true});
-    cy.get('div[role="dialog"] tr[class*="ant-table-row"]').eq(0).find('td').eq(6).contains(/^2$/).should('exist');
+    cy.get('div[role="dialog"] tr[class*="ant-table-row"]').eq(0).find('td').eq(7).contains(/^2$/).should('exist');
     cy.get('div[role="dialog"] thead[class="ant-table-thead"]').contains('# Exons').clickAndWait({force: true});
-    cy.get('div[role="dialog"] tr[class*="ant-table-row"]').eq(0).find('td').eq(6).contains(/^10$/).should('exist');
+    cy.get('div[role="dialog"] tr[class*="ant-table-row"]').eq(0).find('td').eq(7).contains(/^10$/).should('exist');
   });
 
   it('Valider les fonctionnalités du tableau - Tri % Gène', () => {
     cy.get('tr').contains(/^2$/).clickAndWait({force: true});
 
     cy.get('div[role="dialog"] thead[class="ant-table-thead"]').contains('% Gène').clickAndWait({force: true});
-    cy.get('div[role="dialog"] tr[class*="ant-table-row"]').eq(0).find('td').eq(7).contains('10.1 %').should('exist');
+    cy.get('div[role="dialog"] tr[class*="ant-table-row"]').eq(0).find('td').eq(8).contains('10.1 %').should('exist');
     cy.get('div[role="dialog"] thead[class="ant-table-thead"]').contains('% Gène').clickAndWait({force: true});
-    cy.get('div[role="dialog"] tr[class*="ant-table-row"]').eq(0).find('td').eq(7).contains('53.2 %').should('exist');
+    cy.get('div[role="dialog"] tr[class*="ant-table-row"]').eq(0).find('td').eq(8).contains('53.2 %').should('exist');
   });
 
   it('Valider les fonctionnalités du tableau - Tri % CNV', () => {
     cy.get('tr').contains(/^2$/).clickAndWait({force: true});
 
     cy.get('div[role="dialog"] thead[class="ant-table-thead"]').contains('% CNV').clickAndWait({force: true});
-    cy.get('div[role="dialog"] tr[class*="ant-table-row"]').eq(0).find('td').eq(8).contains('24.2 %').should('exist');
+    cy.get('div[role="dialog"] tr[class*="ant-table-row"]').eq(0).find('td').eq(9).contains('24.2 %').should('exist');
     cy.get('div[role="dialog"] thead[class="ant-table-thead"]').contains('% CNV').clickAndWait({force: true});
-    cy.get('div[role="dialog"] tr[class*="ant-table-row"]').eq(0).find('td').eq(8).contains('68.2 %').should('exist');
+    cy.get('div[role="dialog"] tr[class*="ant-table-row"]').eq(0).find('td').eq(9).contains('68.2 %').should('exist');
   });
 
   it('Valider les fonctionnalités du tableau - Tri Type', () => {
     cy.get('tr').contains(/^38$/).clickAndWait({force: true});
 
     cy.get('div[role="dialog"] thead[class="ant-table-thead"]').contains('Type').clickAndWait({force: true});
-    cy.get('div[role="dialog"] tr[class*="ant-table-row"]').eq(0).find('td').eq(9).find('path[d*="M12.4"]').should('exist'); // Gène recoupe le CNV à une de ses extrémités
-    cy.get('div[role="dialog"] tr[class*="ant-table-row"]').eq(0).find('td').eq(9).find('path[d*="M15.6"]').should('exist');
+    cy.get('div[role="dialog"] tr[class*="ant-table-row"]').eq(0).find('td').eq(10).find('path[d*="M12.4"]').should('exist'); // Gène recoupe le CNV à une de ses extrémités
+    cy.get('div[role="dialog"] tr[class*="ant-table-row"]').eq(0).find('td').eq(10).find('path[d*="M15.6"]').should('exist');
     cy.get('div[role="dialog"] thead[class="ant-table-thead"]').contains('Type').clickAndWait({force: true});
-    cy.get('div[role="dialog"] tr[class*="ant-table-row"]').eq(0).find('td').eq(9).find('path[d*="M10.8"]').should('exist'); // Gène entièrement contenu dans le CNV
-    cy.get('div[role="dialog"] tr[class*="ant-table-row"]').eq(0).find('td').eq(9).find('path[d*="M13.2"]').should('exist');
+    cy.get('div[role="dialog"] tr[class*="ant-table-row"]').eq(0).find('td').eq(10).find('path[d*="M10.8"]').should('exist'); // Gène entièrement contenu dans le CNV
+    cy.get('div[role="dialog"] tr[class*="ant-table-row"]').eq(0).find('td').eq(10).find('path[d*="M13.2"]').should('exist');
   });
 
   it('Valider les fonctionnalités du tableau - Tri multiple', () => {
