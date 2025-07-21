fa59944f904e6cb2d20e3b513f2fccb8febe3ad3
Emmanuel Nau <emmanuel.nau.hsj@ssss.gouv.qc.ca>
2025-07-21 14:40:23 -0400

    fix: CLIN-4848 Remove Clinvar table

--- src/views/Snv/Entity/SummaryPanel/Clinical/index.tsx
+++ src/views/Snv/Entity/SummaryPanel/Clinical/index.tsx
@@ -1,13 +1,12 @@
 import intl from 'react-intl-universal';
-import ExternalLink from '@ferlab/ui/core/components/ExternalLink';
 import { Space, Table, Typography } from 'antd';
 import { useTabClinicalData } from 'graphql/variants/tabActions';
 import NoData from 'views/Snv/Entity/NoData';
 
 import CollapsePanel from 'components/containers/collapse';
 
-import { columnsClinVar, columnsPhenotypes } from './columns';
-import { makeClinVarRows, makeGenesOrderedRow } from './utils';
+import { columnsPhenotypes } from './columns';
+import { makeGenesOrderedRow } from './utils';
 
 import styles from './index.module.css';
 
@@ -19,13 +18,7 @@ const { Title } = Typography;
 
 const ClinicalCard = ({ locus }: OwnProps) => {
   const { loading, data } = useTabClinicalData(locus);
-
-  const dataClinvar = data?.clinvar || {};
-  const clinvarId = dataClinvar.clinvar_id;
   const dataGenes = data?.genes || {};
-
-  const clinVarRows = makeClinVarRows(dataClinvar);
-  const clinVarHasRows = clinVarRows.length > 0;
 
   const genesRows = makeGenesOrderedRow(dataGenes);
   const genesHasRows = genesRows.length > 0;
@@ -34,41 +27,6 @@ const ClinicalCard = ({ locus }: OwnProps) => {
     <>
       <Title level={3}>{intl.get('screen.variantDetails.summaryTab.clinicalCardTitle')}</Title>
       <Space direction="vertical" className={styles.clinicalCard} size={16}>
-        <CollapsePanel
-          header={
-            <Space size="middle">
-              <Title level={4}>ClinVar</Title>
-              {clinvarId ? (
-                <ExternalLink
-                  onClick={(e: { stopPropagation: () => any }) => e.stopPropagation()}
-                  className={styles.externalLink}
-                  href={`https://www.ncbi.nlm.nih.gov/clinvar/variation/${clinvarId}`}
-                  hasIcon={true}
-                  data-cy={`ClinicalCard_ClinVar_${clinvarId}_ExternalLink`}
-                >
-                  {clinvarId}
-                </ExternalLink>
-              ) : (
-                ''
-              )}
-            </Space>
-          }
-          loading={loading}
-          datacy="ClinicalCard_ClinVar"
-        >
-          {clinVarHasRows ? (
-            <Table
-              pagination={false}
-              dataSource={clinVarRows}
-              columns={columnsClinVar}
-              bordered
-              size="small"
-              rowKey={(record) => `ClinicalCard_ClinVar_${record.key}`}
-            />
-          ) : (
-            <NoData />
-          )}
-        </CollapsePanel>
         <CollapsePanel
           header={
             <Title level={4}>
